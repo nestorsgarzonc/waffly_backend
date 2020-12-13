e53e04aacefecdbfd4f3a9326a6492c43ac4042e
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2020-12-12 21:39:19 -0500

    added token

--- src/middleware/auth_token.js
+++ src/middleware/auth_token.js
@@ -1,1 +1,12 @@
-//TODO: add token
+import jwt from 'jsonwebtoken'
+
+exports.checkToken = (req, res, next) => {
+    let token = req.get('token')
+    jwt.verify(token, process.env.SEED, (err, decoded) => {
+        if (err) {
+            return res.status(400).json({ ok: false, message: 'Token no valido', err })
+        }
+        req.usuario = decoded.user
+        next()
+    })
+}
--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,15 +1,18 @@
 import { Router } from 'express'
+
 import * as userController from '../controllers/user_controller'
+import * as token from '../middleware/auth_token'
+
 const router = Router()
 
 router.get('/', userController.findAllUsers)
 
 router.get('/:id', userController.findUserById)
 
-router.put('/:id', userController.updateUser)
+router.put('/:id', [token.checkToken], userController.updateUser)
 
-router.put('/addService/:id', userController.addToServiceHistory)
+router.put('/addService/:id', [token.checkToken], userController.addToServiceHistory)
 
-router.delete('/:id', userController.deleteUser)
+router.delete('/:id', [token.checkToken], userController.deleteUser)
 
 export default router;
