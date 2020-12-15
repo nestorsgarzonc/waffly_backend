a2444cfe9664a5ed6644482c0395a1784de1c418
Nestor Sebastian Garzon Contreras <negarzonc@unal.edu.co>
2020-12-14 21:25:21 -0500

    fix freelancer controller and isolate objects

--- src/controllers/freelancer_controller.js
+++ src/controllers/freelancer_controller.js
@@ -1,15 +1,16 @@
 import Freelancer from '../models/Freelancer'
-import Service from '../models/Service'
 import _ from 'underscore'
-import { validationResult } from 'express-validator'
 
 exports.findAllFreelancers = async (__, res) => {
-    Freelancer.find().exec((err, freelancers) => {
-        if (err) {
-            return res.status(400).json({ ok: false, message: err })
-        }
-        res.json({ ok: true, ...freelancers })
-    })
+    Freelancer
+        .find()
+        .populate('services')
+        .exec((err, freelancers) => {
+            if (err) {
+                return res.status(400).json({ ok: false, message: err })
+            }
+            res.json({ ok: true, ...freelancers })
+        })
 }
 
 exports.findFreelancerById = async (req, res) => {
@@ -41,36 +42,6 @@ exports.updateFreelancer = async (req, res) => {
             return res.status(400).json({ ok: false, err })
         }
         res.json({ ok: true, message: 'Freelancer actualizado correctamente' })
-    })
-}
-
-exports.addService = async (req, res) => {
-    const errors = validationResult(req)
-    if (errors.errors.length > 0) {
-        return res.status(422).json({ ok: false, message: errors.errors })
-    }
-    let service = Service({
-        title: req.body.title,
-        category: req.body.category,
-        is_presencial: req.body.is_presencial,
-        description: req.body.description,
-        price: req.body.price,
-        img: req.body.img,
-        freelancer_id: req.params.id,
-    })
-    service.save((err, service_saved) => {
-        if (err) {
-            return res.status(400).json({ ok: false, message: err })
-        }
-        Freelancer.findByIdAndUpdate(
-            req.params.id,
-            { $push: { services: service_saved._id } },
-            (err_f, __) => {
-                if (err_f) {
-                    return res.status(400).json({ ok: false, message: err_f })
-                }
-                res.json({ ok: true, message: 'Servicio agregado correctamente' })
-            })
     })
 }
 
--- src/routes/freelancer.routes.js
+++ src/routes/freelancer.routes.js
@@ -1,5 +1,4 @@
 import { Router } from 'express'
-import { body } from 'express-validator'
 
 import * as freelancerController from '../controllers/freelancer_controller'
 import * as token from '../middleware/auth_token'
@@ -11,16 +10,6 @@ router.get('/', freelancerController.findAllFreelancers)
 router.get('/:id', freelancerController.findFreelancerById)
 
 router.put('/:id', [token.checkToken], freelancerController.updateFreelancer)
-
-//Id of the freelancer
-router.post(
-    '/addService/:id',
-    [
-        token.checkToken,
-        body('img').isURL().withMessage('Ingresa un link valido'),
-    ],
-    freelancerController.addService
-)
 
 router.delete('/:id', [token.checkToken], freelancerController.deleteFreelancer)
 
